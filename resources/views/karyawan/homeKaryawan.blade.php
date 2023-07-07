654184b59cbb6672c3b96e1f24e52860c5d6cc9d
Firgisotya <zzuuddiinn28@gmail.com>
2023-07-07 12:17:44 +0700

    update

--- resources/views/karyawan/homeKaryawan.blade.php
+++ resources/views/karyawan/homeKaryawan.blade.php
@@ -1,80 +1,174 @@
 @extends('layouts.app')
 
 @section('content')
-            <div class="row">
-                <div class="col-4">
-                    <div class="card shadow-lg">
-                        <div class="card-body">
-                            <h5 class="card-title text-primary d-flex justify-content-center">
-                                <i class="fas fa-camera-alt fa-5x"></i>
-                            </h5>
-                            <h2 class="card-text text-primary d-flex justify-content-center">Absen Pagi</h2>
-                        </div>
+    <div class="row">
+        <div class="col-4">
+            <!-- Button trigger modal -->
+            <div data-bs-toggle="modal" data-bs-target="#pagi">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h5 class="card-title text-primary d-flex justify-content-center">
+                            <i class="fas fa-camera-alt fa-5x"></i>
+                        </h5>
+                        <h2 class="card-text text-primary d-flex justify-content-center">Absen Pagi</h2>
                     </div>
                 </div>
-                <div class="col-4">
-                    <div class="card shadow-lg">
-                        <div class="card-body">
-                            <h5 class="card-title text-primary d-flex justify-content-center">
-                                <i class="fas fa-camera-alt fa-5x"></i>
-                            </h5>
-                            <h2 class="card-text text-primary d-flex justify-content-center">Absen Siang</h2>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="pagi" tabindex="-1" aria-labelledby="pagiLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Absen Pagi</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="card shadow-lg">
-                        <div class="card-body">
-                            <h5 class="card-title text-primary d-flex justify-content-center">
-                                <i class="fas fa-camera-alt fa-5x"></i>
-                            </h5>
-                            <h2 class="card-text text-primary d-flex justify-content-center">Absen Sore</h2>
+                        <div class="modal-body">
+                            <form method="POST" action="">
+                                @csrf
+                                        <div id="my_camera"></div>
+                                        <br />
+                                        <input type=button value="Take Snapshot" onClick="take_snapshot()">
+                                        <input type="hidden" name="image" class="image-tag">
+                                    
+                                    <div class="col">
+                                        <div id="results">Your captured image will appear here...</div>
+                                    </div>
+                                    <div class="col-md-12 text-center">
+                                        <br />
+                                        <button class="btn btn-success">Submit</button>
+                                    </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col">
-                    <form method="POST" action="">
-                        @csrf
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div id="my_camera"></div>
-                                <br/>
-                                <input type=button value="Take Snapshot">
-                                <input type="hidden" name="image" class="image-tag">
-                            </div>
-                            <div class="col-md-6">
-                                <div id="results">Your captured image will appear here...</div>
-                            </div>
-                            <div class="col-md-12 text-center">
-                                <br/>
-                                <button class="btn btn-success">Submit</button>
-                            </div>
-                        </div>
-                    </form>
+
+        </div>
+        <div class="col-4">
+            <!-- Button trigger modal -->
+            <div data-bs-toggle="modal" data-bs-target="#siang">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h5 class="card-title text-primary d-flex justify-content-center">
+                            <i class="fas fa-camera-alt fa-5x"></i>
+                        </h5>
+                        <h2 class="card-text text-primary d-flex justify-content-center">Absen Siang</h2>
+                    </div>
                 </div>
             </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="siang" tabindex="-1" aria-labelledby="siangLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Absen Siang</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="POST" action="">
+                                @csrf
+                                        <div id="my_camera"></div>
+                                        <br />
+                                        <input type=button value="Take Snapshot" onClick="take_snapshot()">
+                                        <input type="hidden" name="image" class="image-tag">
+                                    
+                                    <div class="col">
+                                        <div id="results">Your captured image will appear here...</div>
+                                    </div>
+                                    <div class="col-md-12 text-center">
+                                        <br />
+                                        <button class="btn btn-success">Submit</button>
+                                    </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="col-4">
+            <!-- Button trigger modal -->
+            <div data-bs-toggle="modal" data-bs-target="#sore">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h5 class="card-title text-primary d-flex justify-content-center">
+                            <i class="fas fa-camera-alt fa-5x"></i>
+                        </h5>
+                        <h2 class="card-text text-primary d-flex justify-content-center">Absen Sore</h2>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="sore" tabindex="-1" aria-labelledby="soreLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Absen Sore</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="POST" action="">
+                                @csrf
+                                        <div id="my_camera"></div>
+                                        <br />
+                                        <input type=button value="Take Snapshot" onClick="take_snapshot()">
+                                        <input type="hidden" name="image" class="image-tag">
+                                    
+                                    <div class="col">
+                                        <div id="results">Your captured image will appear here...</div>
+                                    </div>
+                                    <div class="col-md-12 text-center">
+                                        <br />
+                                        <button class="btn btn-success">Submit</button>
+                                    </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+   
 
 @section('script')
-<script language="JavaScript">
-    Webcam.set({
-        width: 490,
-        height: 350,
-        image_format: 'jpeg',
-        jpeg_quality: 90
-    });
+    <script>
+        Webcam.set({
+            width: 490,
+            height: 350,
+            image_format: 'jpeg',
+            jpeg_quality: 90
+        });
 
-    Webcam.attach( '#my_camera' );
+        Webcam.attach('#pagi');
+        Webcam.attach('#siang');
+        Webcam.attach('#sore');
 
-    // function take_snapshot() {
-    //     Webcam.snap( function(data_uri) {
-    //         $(".image-tag").val(data_uri);
-    //         document.getElementById('results').innerHTML = '<img src="'+data_uri+'"/>';
-    //     } );
-    // }
-</script>
+        function take_snapshot() {
+            Webcam.snap(function(data_uri) {
+                $(".image-tag").val(data_uri);
+                document.getElementById('results').innerHTML = '<img src="' + data_uri + '"/>';
+            });
+        }
+    </script>
 @endsection
-
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,52 +2,56 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Modernize Free</title>
-  <link rel="shortcut icon" type="image/png" href="{{ asset('images/logos/favicon.png') }}" />
-  <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Modernize Free</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/logos/favicon.png') }}" />
+    <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 
-  {{-- sweetalert --}}
-  <link rel="stylesheet" href="{{ asset('vendor/sweetalert/sweetalert.css') }}">
+    {{-- sweetalert --}}
+    <link rel="stylesheet" href="{{ asset('vendor/sweetalert/sweetalert.css') }}">
+
+    {{-- jquery and webcam --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
 
 </head>
 
 <body>
-  <!--  Body Wrapper -->
-  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-    data-sidebar-position="fixed" data-header-position="fixed">
-    <!-- Sidebar Start -->
-    @include('layouts.sidebar')
-    <!--  Sidebar End -->
-    <!--  Main wrapper -->
-    <div class="body-wrapper">
-      <!--  Header Start -->
-      @include('layouts.navbar')
-      <!--  Header End -->
-      <div class="container-fluid">
-        {{-- render konten --}}
-        @yield('content')
-        {{-- end render konten --}}
-        {{-- footer --}}
-            @include('layouts.footer')
-        {{-- end footer --}}
-      </div>
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <!-- Sidebar Start -->
+        @include('layouts.sidebar')
+        <!--  Sidebar End -->
+        <!--  Main wrapper -->
+        <div class="body-wrapper">
+            <!--  Header Start -->
+            @include('layouts.navbar')
+            <!--  Header End -->
+            <div class="container-fluid">
+                {{-- render konten --}}
+                @yield('content')
+                {{-- end render konten --}}
+                {{-- footer --}}
+                @include('layouts.footer')
+                {{-- end footer --}}
+            </div>
+        </div>
     </div>
-  </div>
-  <script src="{{ asset('libs/jquery/dist/jquery.min.js') }}"></script>
-  <script src="{{ asset('libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('js/sidebarmenu.js') }}"></script>
-  <script src="{{ asset('js/app.min.js') }}"></script>
-  <script src="{{ asset('libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-  <script src="{{ asset('libs/simplebar/dist/simplebar.js') }}"></script>
-  <script src="{{ asset('js/dashboard.js') }}"></script>
+    <script src="{{ asset('libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('js/app.min.js') }}"></script>
+    <script src="{{ asset('libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('libs/simplebar/dist/simplebar.js') }}"></script>
+    <script src="{{ asset('js/dashboard.js') }}"></script>
 
-   {{-- SweetAlert --}}
-   <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
-  @yield('script')
-  
+    {{-- SweetAlert --}}
+    <script src="{{ asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
+    @yield('script')
+
 </body>
 
 </html>
